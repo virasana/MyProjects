618cf74619c0f64ca17eb5958870eeb46d229453
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2017-06-12 12:50:54 +0100

    Resolver

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -1,29 +1,45 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
-import { EventsAppComponent } 
-    from './events-app.component'
-import { EventsListComponent } 
-    from './events/events-list.component'
-import { EventThumbnailComponent } 
-    from './events/event-thumbnail.component'
-import { NavBarComponent } 
-    from './nav/navbar.component'
-import { EventService } 
-    from './events/shared/event.service'
-import { ToastrService } 
-    from './common/toastr.service'
+import { EventsAppComponent } from './events-app.component'
+import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
+import { EventDetailsComponent } from './events/event-details/event-details.component'
+import { appRoutes } from './routes';
+import { RouterModule } from '@angular/router';
+import { CreateEventComponent } from './events/create-event.component';
+import { Error404Component } from './errors/404.errors.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
     declarations: [
-        EventsAppComponent, 
-        EventsListComponent, 
+        EventsAppComponent,
+        EventsListComponent,
         EventThumbnailComponent,
-        NavBarComponent
-        ],
-    providers: [EventService, ToastrService],
+        NavBarComponent,
+        EventDetailsComponent,
+        CreateEventComponent,
+        Error404Component
+    ],
+    providers: [
+        EventService, ToastrService, EventRouteActivator, EventListResolver,
+        { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
+    ],
     bootstrap: [EventsAppComponent]
 
 })
-export class AppModule {}
+export class AppModule { }
+
+function checkDirtyState(component: CreateEventComponent) {
+    if (component.isDirty) {
+        return window.confirm('You have not saved this component.  Do you really want to cancel?');
+    }
+    return true;
+}
--- ng2-fundamentals/app/events-app.component.ts
+++ ng2-fundamentals/app/events-app.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core'
     selector: 'events-app',
     template: `
     <navbar></navbar>
-    <events-list></events-list>
+    <router-outlet></router-outlet>
     `
 })
 export class EventsAppComponent {
 
-}
+} 
