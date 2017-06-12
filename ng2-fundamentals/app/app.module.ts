12c2a9a02d19ddd67872a68ce3233b9a026eadb6
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2017-06-12 15:19:11 +0100

    Organising imports

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { appRoutes } from './routes';
+import { AppRoutes } from './routes';
 import { RouterModule } from '@angular/router';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.errors.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.service';
+import { EventsAppComponent } from './events-app.component'
+
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
+
 
 @NgModule({
     imports: [
         BrowserModule,
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(AppRoutes)
     ],
     declarations: [
         EventsAppComponent,
