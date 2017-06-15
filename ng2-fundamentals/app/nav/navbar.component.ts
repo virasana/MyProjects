775ca297c3587d01fd45551852db54a2c647dbfd
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2017-06-15 10:01:49 +0100

    Implementing the session search

--- ng2-fundamentals/app/nav/navbar.component.ts
+++ ng2-fundamentals/app/nav/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../user/auth.service';
+import { ISession } from '../events/shared/event.model';
+import { EventService } from '../events/shared/event.service';
 
 @Component({
     selector: 'navbar',
@@ -13,9 +15,17 @@ import { AuthService } from '../user/auth.service';
 })
 
 export class NavBarComponent implements OnInit {
-    constructor(private authService: AuthService ) { 
+    constructor(private authService: AuthService, private eventService: EventService ) { 
     
     }
-
+    searchTerm: string = '';
+    foundSessions: ISession[];
     ngOnInit() { }
+
+    searchSessions(searchTerm){
+        this.eventService.searchSessions(searchTerm).subscribe(sessions => {
+            this.foundSessions = sessions;
+            console.log(this.foundSessions);
+        });
+    }
 }
