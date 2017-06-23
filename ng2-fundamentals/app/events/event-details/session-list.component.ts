3640ffdd6bf39b37c7f855738c19c1284e000ef1
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2017-06-23 10:54:38 +0100

    Using DELETE

--- ng2-fundamentals/app/events/event-details/session-list.component.ts
+++ ng2-fundamentals/app/events/event-details/session-list.component.ts
@@ -15,6 +15,7 @@ export class SessionListComponent implements OnInit, OnChanges {
     @Input() sessions: ISession[];
     @Input() filterBy: string;
     @Input() sortBy: string;
+    @Input() eventId: number;
     
     visibleSessions: ISession[] = [];
 
@@ -45,10 +46,10 @@ export class SessionListComponent implements OnInit, OnChanges {
 
     toggleVote(session: ISession){
         if(this.userHasVoted(session)){
-               this.voterService.deleteVoter(session, this.auth.currentUser.userName) ;
+               this.voterService.deleteVoter(this.eventId, session, this.auth.currentUser.userName) ;
         }
         else {
-            this.voterService.addVoter(session, this.auth.currentUser.userName) ;
+            this.voterService.addVoter(this.eventId, session, this.auth.currentUser.userName) ;
         }
 
         if(this.sortBy === 'votes'){
--- ng2-fundamentals/app/events/event-details/voter.service.ts
+++ ng2-fundamentals/app/events/event-details/voter.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import { ISession } from '../shared/index'
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class VoterService{
 
-    deleteVoter(session: ISession, voterName: string){
+    constructor(private http: Http) {}
+    deleteVoter(eventId: number, session: ISession, voterName: string){
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
         session.voters = session.voters.filter(voter => voter !== voterName);
+        this.http.delete(url).catch(this.handleError)
+        .subscribe();
     }
 
-    addVoter(session: ISession, voterName: string){
+    addVoter(eventId: number, session: ISession, voterName: string){
         session.voters.push(voterName);
+
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+ 
+        this.http.post(url, JSON.stringify({} ))
+        .catch(this.handleError)
+        .subscribe(); 
     }
 
     userHasVoted(session: ISession, voterName: string){
         return session.voters.some(v => v === voterName);
+    }
+
+     private handleError(error: Response) {
+        return Observable.throw(error.statusText);
     }
 }
