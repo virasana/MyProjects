614cdd932f7a1d238cc05e0dd4533a4487d8e9d1
director@sharpit.co.za <jeanpierrefouche@gmail.com>
2017-06-23 16:40:11 +0100

    Testing services

--- ng2-fundamentals/app/events/event-details/voter.service.ts
+++ ng2-fundamentals/app/events/event-details/voter.service.ts
@@ -22,7 +22,7 @@ export class VoterService{
         
         let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
  
-        this.http.post(url, JSON.stringify({} ))
+        this.http.post(url, JSON.stringify({} ), options)
         .catch(this.handleError)
         .subscribe(); 
     }
